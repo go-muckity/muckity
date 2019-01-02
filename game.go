7a5bdc8b116744464be967a656f51eddb68e314d
michael k <mking@arikel.net>
2019-01-02 18:29:42 -0500

    better basic game.go

--- game.go
+++ game.go
@@ -3,13 +3,77 @@ package main
 import (
 	"fmt"
 	"github.com/tsal/muckity/pkg/muckity"
+	"os"
+	"strconv"
 )
 
+type gameConfig struct {
+	dbUser string
+	dbPwd string
+	dbHost string
+	dbPort uint
+	dbName string
+}
+
+var defaultConfig *gameConfig
+
+func (gc *gameConfig) asURI() string {
+	return fmt.Sprintf("mongodb://%v:%v@%v:%v/%v",
+			gc.dbUser,
+			gc.dbPwd,
+			gc.dbHost,
+			gc.dbPort,
+			gc.dbName)
+}
+
+func GetConfig() (*gameConfig, error) {
+	gc := new(gameConfig)
+	if value, ok := os.LookupEnv("MUCKITY_DB_USERNAME"); ok {
+		gc.dbUser = value
+	} else {
+		gc.dbUser = "muckity"
+	}
+	if value, ok := os.LookupEnv("MUCKITY_DB_PWD"); ok {
+		if gc.dbUser == "" {
+			panic("Empty MUCKITY_DB_USERNAME but MUCKITY_DB_PWD is set!")
+		}
+		gc.dbPwd = value
+	} else {
+		gc.dbPwd = "muckity"
+	}
+	if value, ok := os.LookupEnv("MUCKITY_DB_HOST"); ok {
+		gc.dbHost = value
+	} else {
+		gc.dbHost = "localhost"
+	}
+	if value, ok := os.LookupEnv("MUCKITY_DB_PORT"); ok {
+		cfgPort, err := strconv.ParseUint(value, 10, 0)
+		if err != nil {
+			panic(err)
+		}
+		gc.dbPort = uint(cfgPort)
+	} else {
+		gc.dbPort = 27017
+	}
+	if value, ok := os.LookupEnv("MUCKITY_DB_NAME"); ok {
+		gc.dbName = value
+	} else {
+		gc.dbName = "muckity"
+	}
+	return gc, nil
+}
+
+func init() {
+	var initCfg, err = GetConfig()
+	if err != nil {
+		panic(err)
+	}
+	defaultConfig = initCfg
+}
+
 func main() {
-	url := "mongodb://muckity:muckity@mongo:27017/muckity"
-	fmt.Println("Attempting to grab url: ", url)
-	storage := muckity.NewMuckityStorage(url)
-	fmt.Println(storage.Client.Database("muckity"))
+	uri := defaultConfig.asURI()
+	muckity.NewMuckityStorage(uri)
 	w, err := muckity.NewWorld("A Brand New World")
 	if err != nil {
 		panic(err)
