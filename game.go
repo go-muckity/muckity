7de31f404aaf7b6b93e60d6ba2ad3946cf2af842
michael k <mking@arikel.net>
2019-01-02 21:48:38 -0500

    updated default game to show example custom world

--- game.go
+++ game.go
@@ -2,14 +2,66 @@ package main
 
 import (
 	"fmt"
+	"github.com/machiel/slugify"
+	"github.com/mongodb/mongo-go-driver/bson"
 	"github.com/tsal/muckity/pkg/muckity"
 )
 
+type myWorld struct {
+	name string
+	description string
+	zones []string
+}
+
+func (w myWorld) Name() string {
+	return w.name
+}
+
+func (w myWorld) Type() string {
+	return "worlds"
+}
+
+func (w myWorld) DBId() string {
+	return fmt.Sprintf("world:%v", slugify.Slugify(w.name))
+}
+
+func (w myWorld) Metadata() interface{} {
+	return w.PersistentData()
+}
+func (w myWorld) PersistentData() interface{} {
+	p := bson.D{
+		{"$set", bson.D{
+			{
+				"name",
+				w.Name(),
+			},
+			{
+				"description",
+				w.description,
+			},
+			{
+				"zones",
+				w.zones,
+			},
+		}},
+	}
+	return p
+}
+
+
 func main() {
-	muckity.GetMuckityStorage()
-	w, err := muckity.NewWorld("Development World")
+	storage := muckity.GetMuckityStorage()
+	w := new(myWorld)
+	w.name = "Descriptive World"
+	w.description = `I am a really descriptive world.
+I'm using a custom struct that implements the Persistent interface.
+`
+	w.zones = make([]string,0)
+	res, err := storage.Save(w)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("ID\t: ", w.Name())
+	if value, ok := res.(myWorld); ok {
+		fmt.Println("ID\t: ", value.DBId())
+	}
 }
