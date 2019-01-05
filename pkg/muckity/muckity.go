8595123a41e3fe3a0c4eb61bbba6f783f4589126
michael k <mking@arikel.net>
2019-01-05 00:17:54 -0500

    refactor for sanity

--- pkg/muckity/muckity.go
+++ pkg/muckity/muckity.go
@@ -1,30 +1,46 @@
 package muckity
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
-type muckityRoot interface {
+// MuckityType is used for information discovery
+type MuckityType interface {
+	// Name should be the short name of the object; not necessarily a slug
 	Name() string
+	// Type should be the name of the type for storage purposes.
+	// It should include two or more parts, separated by colon (:) as such:
+	// `muckity:example`; this is to indicate it's defined in the muckity package
+	// and is a type of "example".  This is only used as metadata, not runtime.
 	Type() string
 }
 
-type muckityObject interface {
-	Aliases() []string
-}
-
-// MuckityObject is the basic object in the Muckity ECS
-type MuckityObject interface {
-	muckityRoot
-	muckityObject
-}
-
-type muckitySystem interface {
-	Channels() map[string]chan interface{}
-}
-
 type MuckitySystem interface {
-	muckityRoot
-	muckitySystem
+	// MuckityContext returns the context for the system in question.
+	// For worlds, it should return a derived child context from the world's active context.
+	// Systems can pass whatever context they want down the chain, as long as it's a child of
+	// the world that spawned the system.
+	// TODO: implement this better; this is not the best use-case for context.Context
+	Context() context.Context
+	MuckityType
 }
+
+type MuckityPersistent interface {
+	// BSON returns bson primitives; should be re-usable to generate JSON, for example
+	BSON() interface{}
+	GetId() string
+	SetId(key string)
+	MuckityType
+
+}
+
+// MuckityStorage implements base storage system for later decoupling
+type MuckityStorage interface {
+	Save(obj MuckityPersistent) error
+	MuckitySystem
+}
+
 
 // Tertia is the next division down from a second; it's also a tick
 const Tertia = time.Second / 60
