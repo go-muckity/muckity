b643cd1b902d9a0671c667b4935cd57d02310871
michael k <mking@arikel.net>
2019-01-05 00:18:28 -0500

    decouple everything.. but re-couple config

--- pkg/muckity/storage.go
+++ pkg/muckity/storage.go
@@ -2,99 +2,132 @@ package muckity
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"github.com/mongodb/mongo-go-driver/bson"
 	"github.com/mongodb/mongo-go-driver/mongo"
+	url2 "net/url"
+	"os"
+	"strings"
 	"time"
 )
 
-// MuckityStorage implements base storage system for later decoupling
-type MuckityStorage struct {
-	Client       *mongo.Client
-	databaseName string
-}
-
-// Persistent interface describes objects / structs that can be saved in storage
-// DBId() should return a string - empty if new; _id key otherwise
-// PersistentData() should return a bson.D{{}} struct or marshal data
-type Persistent interface {
-	DBId() string
-	PersistentData() interface{}
-}
-
-type mongoConfig struct {
-	dbUser string
-	dbPwd  string
-	dbHost string
-	dbPort uint
-	dbName string
-}
-
-func (mc mongoConfig) asURI() string {
-	return fmt.Sprintf("mongodb://%v:%v@%v:%v/%v",
-		mc.dbUser,
-		mc.dbPwd,
-		mc.dbHost,
-		mc.dbPort,
-		mc.dbName)
+type MongoStorage struct {
+	id				interface{}
+	dbUrl			*url2.URL
+	databaseName 	string
+	parentCtx 	context.Context
 }
 
 // Name implements part of MuckitySystem
-func (ms MuckityStorage) Name() string {
-	return fmt.Sprintf("system:mongodb:%v", ms.databaseName)
+func (ms *MongoStorage) Name() string {
+	return fmt.Sprintf("mongodb:%v:%v", ms.dbUrl.Host, ms.dbUrl.Path)
 }
 
 // Type implements part of MuckitySystem
-func (ms MuckityStorage) Type() string {
-	return "systems"
+func (ms *MongoStorage) Type() string {
+	return "muckity:storage"
 }
 
-// Channels implements part of MuckitySystem
-func (ms MuckityStorage) Channels() map[string]chan interface{} {
-	// TODO: implement context handling / closing
-	return make(map[string]chan interface{}, 0)
+func (ms *MongoStorage) Context() context.Context {
+	return ms.parentCtx
+}
+
+func (w *MongoStorage) String() string {
+	return fmt.Sprintf("%v:%v", w.Type(), w.Name())
+}
+
+func (ms MongoStorage) Client() (*mongo.Client, error)  {
+	var (
+		ctx		context.Context
+		client	*mongo.Client
+		err		error
+	)
+
+	ctx, _ = context.WithTimeout(ms.parentCtx, time.Second * 30)
+	client, err = mongo.NewClient(ms.dbUrl.String())
+	err = client.Connect(ctx)
+	return client, err
 }
 
 // Save implements storage persistence for compatible objects
-func (ms *MuckityStorage) Save(obj MuckityObject) (interface{}, error) {
-	if pObj, ok := obj.(Persistent); ok {
-		coll := ms.Client.Database(ms.databaseName).Collection(obj.Type())
-		pd := pObj.PersistentData()
-		opt := newUpsert()
-		filter := bson.D{{
-			"_id",
-			pObj.DBId(),
-		}}
-		res, err := coll.UpdateOne(context.TODO(), filter, pd, &opt)
-		if err != nil {
-			panic(err)
-		}
-		if uid, ok := res.UpsertedID.(string); ok {
-			if uid != pObj.DBId() {
-				return obj, errors.New(fmt.Sprintf("Integrity error, got bad ID: %v", uid))
-			}
-		}
-	} else {
-		return obj, errors.New(fmt.Sprintf("Tried to persist a non-persistent object: %v", obj.Name()))
+func (ms MongoStorage) Save(obj MuckityPersistent) error {
+	var (
+		client	*mongo.Client
+		err 	error
+	)
+	client, err = ms.Client()
+	if err != nil {
+		// Don't even try
+		return err
 	}
-	return obj, nil
+	coll := client.Database(ms.databaseName).Collection("muckity")
+	pd := obj.BSON()
+	opt := newUpsert()
+	filter := bson.D{{
+		"_id",
+		obj.GetId(),
+	}}
+	res, err := coll.UpdateOne(ms.Context(), filter, pd, &opt)
+	if err != nil {
+		panic(err)
+	}
+	if uid, ok := res.UpsertedID.(string); ok {
+		obj.SetId(uid)
+	}
+	return err
 }
 
-func dbInit(done chan interface{}) *MuckityStorage {
-	muckCfg := core.Config()
-	client, err := mongo.NewClient(muckCfg.mongo.asURI())
-	if err != nil {
-		panic(err)
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel()
-	err = client.Connect(ctx)
-	if err != nil {
-		panic(err)
-	}
-	storage := new(MuckityStorage)
-	storage.Client = client
-	storage.databaseName = muckCfg.mongo.dbName
-	return storage
+func NewMongoStorage(ctx context.Context) *MongoStorage {
+	var (
+		ms 	MongoStorage
+		url	*url2.URL
+	)
+	url = parseConfig()
+	pathSplit := strings.Split(url.Path, "/")
+	path := pathSplit[len(pathSplit)-1]
+	ms = MongoStorage{nil, url, path, ctx }
+	return &ms
 }
+
+// TODO: use a real config service
+func parseConfig() *url2.URL {
+	var (
+		dbUser	string
+		dbPwd	string
+		dbHost	string
+		dbName	string
+		url 	*url2.URL
+	)
+
+	if value, ok := os.LookupEnv("MUCKITY_DB_USERNAME"); ok {
+		dbUser = value
+	} else {
+		dbUser = "muckity"
+	}
+	if value, ok := os.LookupEnv("MUCKITY_DB_PWD"); ok {
+		dbPwd = value
+	} else {
+		dbPwd = "muckity"
+	}
+	if value, ok := os.LookupEnv("MUCKITY_DB_HOST"); ok {
+		dbHost = value
+	} else {
+		dbHost = "localhost"
+	}
+	if value, ok := os.LookupEnv("MUCKITY_DB_PORT"); ok {
+		dbHost = fmt.Sprintf("%v:%v", dbHost, value)
+	} else {
+		dbHost = fmt.Sprintf("%v:27017", dbHost)
+	}
+	if value, ok := os.LookupEnv("MUCKITY_DB_NAME"); ok {
+		dbName = value
+	} else {
+		dbName = "muckity"
+	}
+	url, err := url2.Parse(fmt.Sprintf("mongodb://%v:%v@%v/%v", dbUser, dbPwd, dbHost, dbName))
+	if err != nil {
+		panic(err)
+	}
+	return url
+}
+
